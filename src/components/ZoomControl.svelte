0b95f4d7c140137b454d603d3e22eee117ca6fc2
schlesingermatthias <schlesinger.matthias@gmail.com>
2021-07-26 12:54:10 +0200

    Create ZoomControl.svelte

---
+++ src/components/ZoomControl.svelte
@@ -0,0 +1,23 @@
+<script>
+    import {getContext, onDestroy} from 'svelte';
+    import L from 'leaflet';
+    
+    const {getMap} = getContext(L);
+    
+    export let position = 'topleft';
+    export let options = {};
+    
+    let zoomControl;
+    $: {
+        if (!zoomControl) {
+            zoomControl = L.control.zoom(options).addTo(getMap());
+        }
+        zoomControl.setPosition(position);
+    }
+    onDestroy(() => {
+        zoomControl.removeFrom(getMap());
+    });
+    export function getZoomControl() {
+        return zoomControl;
+    }
+</script>
